727e409e6330f9eac122ad87d37ed8493f65e743
KCasperXR <kim.casper@outlook.com>
2021-09-04 17:00:26 +0800

    refactor: added props

--- src/components/TextContentBox/index.js
+++ src/components/TextContentBox/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './index.css';
 
-const TextContentBox = ({ para, title }) => {
+const TextContentBox = ({ para, paraTwo, paraThree, title }) => {
   return (
     <article className='text-content-box'>
       <div>
         <h1>{title}</h1>
         <p>{para}</p>
+        <p>{paraTwo}</p>
+        <p>{paraThree}</p>
       </div>
     </article>
   );
